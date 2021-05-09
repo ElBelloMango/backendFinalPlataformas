fa1f666a000b2881ca42f26ecab5a79d0cd7e038
ElBelloMango <miguel.gallego@upb.edu.co>
2021-05-09 17:25:10 -0500

    Exportando # 5: views

---
+++ backEnd/resources/views/personajes.blade.php
@@ -0,0 +1,204 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html lang="zxx">
+
+<head>
+	<title>Shingeki No Kyojin</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Shingeki No Kyojin, Flat Web Templates, Android Compatible web template, 
+SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<!-- Custom CSS -->
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<!-- font-awesome icons -->
+	<link href="css/fontawesome-all.min.css" rel="stylesheet">
+	<!-- //Custom Theme files -->
+	<!--webfonts-->
+	<link href="//fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
+	<!--//webfonts-->
+</head>
+
+<body>
+	<!-- header -->
+	<script src="js/referencias.js"></script>
+	<!-- <script src="js/rotadorBanners.js"></script> -->
+	<header>
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<a class="navbar-brand" href="javascript:void(0)">
+					SNK
+				</a>
+				<button class="navbar-toggler ml-lg-auto ml-sm-5" type="button" data-toggle="collapse"
+					data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<!-- Login -->
+				
+				<!-- Login -->
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav text-center ml-auto">
+						<li class="nav-item mr-3">
+							<a class="nav-link" id="index.html" href="javascript:void(0)">Inicio</a>
+						</li>
+						<li class="nav-item  mr-3">
+							<a class="nav-link scroll" id="acercade.html" href="javascript:void(0)">Informacion</a>
+						</li>
+						<li class="nav-item dropdown active mr-3">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+								data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Desplegable
+							</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item active scroll" id="personajes.html"
+									href="javascript:void(0)">Personajes</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item scroll" id="beneficios.html"
+									href="javascript:void(0)">Beneficios</a>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link scroll" id="contacto.html" href="javascript:void(0)">Contacto</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link scroll" id="renderizarAPI.html" href="javascript:void(0)">API</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!-- //header -->
+	<section class="about-w3ls py-md-5 pb-3" id="services">
+		<div class="row">
+			<div class="col-lg-8 section-3">
+				<div class="col-lg-8 mx-auto py-lg-5">
+					<h3 class="stat-title text-white">Personajes más queridos/populares</h3>
+					<div class="row">
+						<div class="col-md-6 agileits-abt-grids my-5">
+							<h4>Erwin Smith</h4>
+							<p class="sec-4">El comandante de las tropas de reconocimiento, el humano más valiente y el
+								primero
+								en sospechar sobre la realidad fuera de las murallas.
+							</p>
+						</div>
+						<div class="col-md-6 agileits-abt-grids my-md-5">
+							<h4>Levi Ackerman</h4>
+							<p class="sec-4">El humano más fuerte, con la sangre Ackerman y un pasado difícil logró ser
+								de los
+								más vitales personajes y de los más queridos.
+							</p>
+						</div>
+						<div class="col-md-6 agileits-abt-grids mt-md-0 mt-5">
+							<h4>Mikasa Ackerman</h4>
+							<p class="sec-4">Carismatica y protectora de Eren, además de fuerte y valiente es de los
+								personajes
+								favoritos de el Anime y Manga.
+							</p>
+						</div>
+						<div class="col-md-6 agileits-abt-grids mt-md-0 mt-5">
+							<h4>Historia Reiss</h4>
+							<p class="sec-4">De sangre real pero oculta toda la vida, Historia se ganó el cariño de los
+								fans
+								al demostrar su valentía.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+				<head>
+					<script src="js/rotadorBanners.js"></script>
+				</head>
+				<body>
+					<div class="col-4 img-section3 p-0 my-lg-auto mx-auto">
+						<img src="" alt="Anuncios" name="anuncio" class="img-fluid">
+					</div>
+				</body>
+		</div>
+	</section>
+	<!-- //services -->
+	<!-- slide -->
+	
+	<div class="slide-wthree">
+		<div class="container">
+			<div class="slide-img">
+				<img src="images/ErenJeager.png" alt="" class="img-fluid">
+			</div>
+			<h3 class="stat-title text-lg-right text-center">EREN JEAGER</h3>
+
+		</div>
+	</div>
+	
+	<!-- copyright -->
+	<div class="cpy-right text-center">
+		<p>Carlos Andres Uribe Cortes:
+			<a href="mailto:carlos.uribec@upb.edu.co">carlos.uribec@upb.edu.co</a>
+		</p>
+		<p>Miguel Angel Gallego Zuleta:
+			<a href="mailto:miguel.gallego@upb.edu.co">miguel.gallego@upb.edu.co</a>
+		</p>
+	</div>
+	<!-- //copyright -->
+	<!-- js-->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- js-->
+	<!-- start-smooth-scrolling -->
+	<script src="js/move-top.js "></script>
+	<script src="js/easing.js "></script>
+	<script>
+		jQuery(document).ready(function ($) {
+			$(".scroll ").click(function (event) {
+				event.preventDefault();
+
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000);
+			});
+		});
+	</script>
+	<!-- //end-smooth-scrolling -->
+	<!-- smooth-scrolling-of-move-up -->
+	<script>
+		$(document).ready(function () {
+			/*
+			 var defaults = {
+				 containerID: 'toTop', // fading element id
+				 containerHoverID: 'toTopHover', // fading element hover id
+				 scrollSpeed: 1200,
+				 easingType: 'linear' 
+			 };
+			 */
+
+			$().UItoTop({
+				easingType: 'easeOutQuart'
+			});
+
+		});
+	</script>
+	<script src="js/SmoothScroll.min.js "></script>
+	<!-- //smooth-scrolling-of-move-up -->
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.js">
+	</script>
+	<!-- //Bootstrap Core JavaScript -->
+</body>
+
+</html>
