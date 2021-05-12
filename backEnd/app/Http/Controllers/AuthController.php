d0a712d026ad459f4389a1d68aab72293c9ea698
Carlos Uribe <carlos.uribec@upb.edu.co>
2021-05-11 20:36:50 -0500

    Funcion registro - AuthController (Registro)

--- backEnd/app/Http/Controllers/AuthController.php
+++ backEnd/app/Http/Controllers/AuthController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
+use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Hash; 
+use App\User;
 
 class AuthController extends Controller
 {
-    //
+    public function registro(Request $request) { 
+        $request->validate([ 
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=> 'required|min:6' 
+        ]);   
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+        $user->save();
+        $token=$user->createToken($user->email.'-'.now());
+        return response()->json(['user'=>$user, 'token'=>$token->accessToken]);
+    }
 }
