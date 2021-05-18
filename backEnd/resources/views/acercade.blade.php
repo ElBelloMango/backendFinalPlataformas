efefefe510c74989da850c7729eb909c7412d59b
ElBelloMango <miguel.gallego@upb.edu.co>
2021-05-17 22:30:46 -0500

    Active re implementado acerca

--- backEnd/resources/views/acercade.blade.php
+++ backEnd/resources/views/acercade.blade.php
@@ -54,12 +54,12 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
 				<!-- Login -->
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav text-center ml-auto">
-					<li class="nav-item active  mr-3">
+					<li class="nav-item mr-3">
 							<a class="nav-link"  id="/" href="javascript:void(0)">Inicio
 								<span class="sr-only">(current)</span>
 							</a>
 						</li>
-						<li class="nav-item  mr-3">
+						<li class="nav-item active mr-3">
 							<a class="nav-link" id="/acercade" href="javascript:void(0)">Informacion</a>
 						</li>
 						<li class="nav-item dropdown mr-3">
