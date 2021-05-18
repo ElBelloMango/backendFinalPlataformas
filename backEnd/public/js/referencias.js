0c766424d4c1919468efa1b059d613b673e58930
ElBelloMango <miguel.gallego@upb.edu.co>
2021-05-17 20:15:55 -0500

    Navegacion bajo logueo 1

--- backEnd/public/js/referencias.js
+++ backEnd/public/js/referencias.js
@@ -13,7 +13,7 @@ function init()
     }
     if( window.location.pathname=="/")
     {
-        logeado=true;
+        logeado=false;
         logeo();       //Se comentó la funcion logueo por que el md5 no funciona en githubpages ya que no soporta php, para el funcionamiento correcto del logeo colocar la variable logueo de la linea 15 en false y quite el comentario en esta linea
     }
     if(window.location.pathname=="/renderizarAPI")
